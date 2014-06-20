27f292208a70db20814bcd4ec46a24c9a80b13fc
mlennon3@gmail.com <mlennon3@gmail.com>
2014-06-20 12:48:04 -0500

    Michael & Valarie & Asif  Removed multiple ifs from PreviewErrorValidator.hasErrors()

--- api/src/main/java/org/openmrs/module/mergeconcepts/api/impl/PreviewErrorValidatorImpl.java
+++ api/src/main/java/org/openmrs/module/mergeconcepts/api/impl/PreviewErrorValidatorImpl.java
@@ -7,6 +7,8 @@ import org.openmrs.module.mergeconcepts.api.PreviewErrorValidator;
 import org.openmrs.web.WebConstants;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PreviewErrorValidatorImpl implements PreviewErrorValidator {
     //if both concepts' types are numeric, make sure both precision (y/n)s are the same
@@ -69,70 +71,49 @@ public class PreviewErrorValidatorImpl implements PreviewErrorValidator {
     }
 
     public boolean hasErrors(Integer oldConceptId, Integer newConceptId, HttpSession httpSession) {
+        List<ErrorBucket> errorBuckets = getErrorBuckets(oldConceptId, newConceptId);
 
-        if (oldConceptId == null || newConceptId == null) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "Please choose two concepts and try again");
-            return true;
+        for (ErrorBucket bucket : errorBuckets) {
+            if(bucket.hasError()) {
+                setErrorMessage(httpSession, bucket.getMessage());
+                return true;
+            }
         }
+        return false;
+    }
 
+    private List<ErrorBucket> getErrorBuckets(Integer oldConceptId, Integer newConceptId) {
         Concept oldConcept = getConceptService().getConcept(oldConceptId);
         Concept newConcept = getConceptService().getConcept(newConceptId);
 
-        //handle conceptIds are the same
-        if (oldConceptId.equals(newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "The same concept was chosen twice - please try again");
-            return true;
-        }
+        boolean anIDIsNull           = oldConceptId == null || newConceptId == null;
+        boolean idsAreTheSame        = oldConceptId.equals(newConceptId);
+        boolean differentDatatypes   = !hasMatchingDatatypes(oldConcept, newConcept);
+        boolean differentAnswerSets  = oldConcept.getDatatype().isCoded() && !hasCodedAnswers(oldConcept, newConcept);
+        boolean retiredIsTooHigh     = isNumeric(oldConcept) && !hasCorrectAbsoluteHi(oldConceptId, newConceptId);
+        boolean retiredIsTooLow      = isNumeric(oldConcept) && !hasCorrectAbsoluteLow(oldConceptId, newConceptId);
+        boolean differentUnits       = isNumeric(oldConcept) && !hasMatchingUnits(oldConceptId, newConceptId);
+        boolean differentPrecision   = isNumeric(oldConcept) && !hasMatchingPrecise(oldConceptId, newConceptId);
+        boolean differentHandlers    = oldConcept.getDatatype().isComplex() && !hasMatchingComplexHandler(oldConceptId, newConceptId);
 
-        //handle concepts with different datatypes
-        //TODO - unless oldConcept is N/A (what if it's the other way around?) <-- is that right?
-        if (!hasMatchingDatatypes(oldConcept, newConcept)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR, "Please choose concepts with same datatypes and try again");
-            return true;
-        }
+        List<ErrorBucket> errors = new ArrayList<ErrorBucket>();
 
-        //if both concepts' types are coded, make sure both answer sets are the same
-        if (oldConcept.getDatatype().isCoded() && !hasCodedAnswers(oldConcept, newConcept)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Concept chosen to be retired has answers that the concept to keep does not have - please try again");
-            return true;
-        }
+        errors.add(new ErrorBucket(anIDIsNull, "Please choose two concepts and try again"));
+        errors.add(new ErrorBucket(idsAreTheSame, "The same concept was chosen twice - please try again"));
+        errors.add(new ErrorBucket(differentDatatypes, "Please choose concepts with same datatypes and try again"));
+        errors.add(new ErrorBucket(differentAnswerSets, "Concept chosen to be retired has answers that the concept to keep does not have - please try again"));
+        errors.add(new ErrorBucket(retiredIsTooHigh, "Absolute high for concept to be retired is greater than absolute high for concept to keep - please try again"));
+        errors.add(new ErrorBucket(retiredIsTooLow, "Absolute low for concept to be retired is less than absolute low for concept to keep - please try again"));
+        errors.add(new ErrorBucket(differentUnits, "Concepts you chose have different units - please try again"));
+        errors.add(new ErrorBucket(differentPrecision, "Concepts do not agree on precise (y/n) - please try again"));
+        errors.add(new ErrorBucket(differentHandlers, "Complex concepts do not have the same handler - please try again"));
 
-        //if both concepts' types are numeric, make sure absolute high for concept to keep includes absolute high for concept to retire
-        if (isNumeric(oldConcept) && !hasCorrectAbsoluteHi(oldConceptId, newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Absolute high for concept to be retired is greater than absolute high for concept to keep - please try again");
-            return true;
-        }
+        return errors;
+    }
 
-        //if both concepts' types are numeric, make sure absolute low for concept to keep includes absolute low for concept to retire
-        if (isNumeric(oldConcept) && !hasCorrectAbsoluteLow(oldConceptId, newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Absolute low for concept to be retired is less than absolute low for concept to keep - please try again");
-            return true;
-        }
-
-        //if both concepts' types are numeric, make sure units are the same
-        if (isNumeric(oldConcept) && !hasMatchingUnits(oldConceptId, newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Concepts you chose have different units - please try again");
-            return true;
-        }
-
-        //if both concepts' types are numeric, make sure both precision (y/n)s are the same
-        if (isNumeric(oldConcept) && !hasMatchingPrecise(oldConceptId, newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Concepts do not agree on precise (y/n) - please try again");
-            return true;
-        }
-
-        //if both concepts' types are complex, make sure handlers are the same
-        if (oldConcept.getDatatype().isComplex() && !hasMatchingComplexHandler(oldConceptId, newConceptId)) {
-            httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
-                    "Complex concepts do not have the same handler - please try again");
-            return true;
-        }
-        return false;
+    private void setErrorMessage(HttpSession httpSession, String message) {
+        httpSession.setAttribute(WebConstants.OPENMRS_ERROR_ATTR,
+                message);
     }
 
     private ConceptService getConceptService() {
@@ -141,5 +122,22 @@ public class PreviewErrorValidatorImpl implements PreviewErrorValidator {
 
     private boolean isNumeric(Concept oldConcept) {
         return oldConcept.getDatatype().isNumeric();
+    }
+    class ErrorBucket {
+        private boolean hasError;
+        private String message;
+
+        public ErrorBucket(boolean hasError, String message) {
+            this.hasError = hasError;
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public boolean hasError() {
+            return hasError;
+        }
     }
 }
