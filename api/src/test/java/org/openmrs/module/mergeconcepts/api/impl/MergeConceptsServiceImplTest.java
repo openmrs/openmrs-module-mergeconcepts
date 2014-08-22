7d59f4ec266395c977a2c76a3bfca46e5e5efb74
mlennon3@gmail.com <mlennon3@gmail.com>
2014-08-22 11:23:49 -0500

    Refactored the two test classes into one

--- api/src/test/java/org/openmrs/module/mergeconcepts/api/impl/MergeConceptsServiceImplTest.java
+++ api/src/test/java/org/openmrs/module/mergeconcepts/api/impl/MergeConceptsServiceImplTest.java
@@ -1,11 +1,23 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.mergeconcepts.api.impl;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openmrs.Concept;
-import org.openmrs.ConceptSet;
+import org.openmrs.*;
+import org.openmrs.api.*;
 import org.openmrs.PersonAttributeType;
-import org.openmrs.api.ConceptService;
 import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.mergeconcepts.api.MergeConceptsService;
@@ -14,11 +26,11 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class MergeConceptsServiceImplTest extends BaseModuleContextSensitiveTest {
+    int FOOD_ASSISTANCE_CONCEPT_ID = 18; // name="FOOD ASSISTANCE"
 
-    private Concept parentConcept;
     private ConceptService conceptService;
     private MergeConceptsService mergeConceptsService;
 
@@ -26,6 +38,7 @@ public class MergeConceptsServiceImplTest extends BaseModuleContextSensitiveTest
     public void setUp() throws Exception {
         mergeConceptsService = Context.getService(MergeConceptsService.class);
         conceptService = Context.getConceptService();
+        executeDataSet(EXAMPLE_XML_DATASET_PACKAGE_PATH );
     }
 
     @Test
@@ -80,7 +93,7 @@ public class MergeConceptsServiceImplTest extends BaseModuleContextSensitiveTest
     }
 
     private void setUpConceptSetTest() {
-        parentConcept = conceptService.getConcept(3);
+        Concept parentConcept = conceptService.getConcept(3);
         Concept childConcept = conceptService.getConcept(4);
 
         parentConcept.addSetMember(childConcept);
@@ -88,4 +101,194 @@ public class MergeConceptsServiceImplTest extends BaseModuleContextSensitiveTest
         conceptService.saveConcept(parentConcept);
     }
 
+
+    @Test
+    public void shouldSetupContext() {
+        assertNotNull(Context.getService(MergeConceptsService.class));
+    }
+
+    @Test
+    public void getObsCount_shouldReturnACountOfQuestionAndAnswerConceptObs()
+            throws Exception {
+        int knownObsId = 7;
+        updateObsWithConceptAnswer(knownObsId, FOOD_ASSISTANCE_CONCEPT_ID);
+
+        Integer actualServiceObsCount = mergeConceptsService.getObsCount(FOOD_ASSISTANCE_CONCEPT_ID);
+
+        Integer expectedServiceObsCount = 2;
+        assertEquals(expectedServiceObsCount, actualServiceObsCount);
+    }
+
+    @Test
+    public void updateObs_shouldUpdateObsWithNewConceptInQuestionsAndAnswerConcepts() {
+        Obs savedObsWithId10 = updateObsWithConceptAnswer(10, FOOD_ASSISTANCE_CONCEPT_ID);
+        Obs savedObsWithId12 = updateObsWithConceptQuestion(12, FOOD_ASSISTANCE_CONCEPT_ID);
+        int newConceptId = 22;
+
+        Concept knownConcept = conceptService.getConcept(FOOD_ASSISTANCE_CONCEPT_ID);
+        Concept newConcept = conceptService.getConcept(newConceptId);
+
+        mergeConceptsService.updateObs(knownConcept, newConcept);
+
+        assertEquals(new Integer(newConceptId), savedObsWithId10.getValueCoded().getId());
+        assertEquals(new Integer(newConceptId), savedObsWithId12.getConcept().getId());
+    }
+
+    @Test
+    public void updateOrders_shouldUpdateOrdersWithNewConcept() throws Exception {
+        int oldConceptId = 23;
+        Order orderWithId4 = updateOrderWithConcept(4, 23);
+        Order orderWithId5 = updateOrderWithConcept(5, 23);
+        int newConceptId = 18;
+
+        Concept oldConcept = conceptService.getConcept(oldConceptId);
+        Concept newConcept = conceptService.getConcept(newConceptId);
+
+        mergeConceptsService.updateOrders(oldConcept,newConcept);
+
+        assertEquals(new Integer(newConceptId), orderWithId4.getConcept().getId());
+        assertEquals(new Integer(newConceptId), orderWithId5.getConcept().getId());
+    }
+
+    @Test
+    public void updateFormFields_shouldUpdateFormFieldAttributes() throws Exception {
+        int newConceptId = 88;
+        int oldConceptId = 3;
+
+        String newName = conceptService.getConcept(newConceptId).getName().toString();
+        String newDescription = conceptService.getConcept(newConceptId).getDescription().toString();
+
+        Concept oldConcept = conceptService.getConcept(oldConceptId);
+        Concept newConcept = conceptService.getConcept(newConceptId);
+
+        mergeConceptsService.updateFields(oldConcept, newConcept);
+
+        int fieldId = 1;
+        FormService formService = Context.getFormService();
+        Field field = formService.getField(fieldId);
+
+        String updatedName = field.getName();
+        String updatedDescription = field.getDescription();
+        int updatedConceptId = field.getConcept().getId();
+
+        assertEquals(newConceptId, updatedConceptId);
+        assertEquals(newName, updatedName);
+        assertEquals(newDescription, updatedDescription);
+    }
+
+    @Test
+    public void updateProgram_shouldUpdateProgramNameDescriptionAndId() throws Exception {
+        //program 2 references concept 10
+        //retire concept 10 in favor of concept 9
+
+        int newConceptId = 9;
+        int oldConceptId = 10;
+
+        ConceptService conceptService = Context.getConceptService();
+        String newName = conceptService.getConcept(newConceptId).getName().toString();
+        String newDescription = conceptService.getConcept(newConceptId).getDescription().toString();
+
+        mergeConceptsService.updatePrograms(Context.getConceptService().getConcept(oldConceptId), Context.getConceptService().getConcept(newConceptId));
+
+        int programId = 2;
+        ProgramWorkflowService programService = Context.getProgramWorkflowService();
+        Program program = programService.getProgram(programId);
+
+        String updatedName = program.getName();
+        String updatedDescription = program.getDescription();
+        int updatedConceptId = program.getConcept().getId();
+
+        assertEquals(newConceptId, updatedConceptId);
+        assertEquals(newName, updatedName);
+        assertEquals(newDescription, updatedDescription);
+    }
+
+
+    @Test
+    public void getDrugsByRouteConcept() throws Exception {
+
+        Concept route = Context.getConceptService().getConcept(22);
+        Drug drugToWhichWeAddRoute = Context.getConceptService().getDrug(11);
+        drugToWhichWeAddRoute.setRoute(route);
+
+        Context.getConceptService().saveDrug(drugToWhichWeAddRoute);
+
+        List<Drug> drugs = mergeConceptsService.getDrugsByRouteConcept(route);
+
+        assertEquals(1, drugs.size());
+        assertEquals( route.getConceptId(), drugs.get(0).getRoute().getConceptId());
+    }
+
+
+    @Test
+    public void shouldUpdateRouteConceptIdInDrug() {
+        Concept oldConceptWithRoute = getConceptFromConceptId(22);
+        Concept newConceptWithRoute = getConceptFromConceptId(23);
+        Drug drug = getDrugById(11);
+        drug.setRoute(oldConceptWithRoute);
+        saveDrugToConceptService(drug);
+
+        MergeConceptsService service = Context.getService(MergeConceptsService.class);
+        service.updateDrugs(oldConceptWithRoute, newConceptWithRoute);
+
+        Integer updatedRouteConceptIdInDrug = drug.getRoute().getConceptId();
+        assertThat(updatedRouteConceptIdInDrug, is(23));
+    }
+
+    @Test
+    public void shouldUpdateDoseFormConceptIdInDrug() {
+        Concept oldConceptWithDosageForm = getConceptFromConceptId(7);
+        Concept newConceptWithDosageForm = getConceptFromConceptId(8);
+        Drug drug = getDrugById(11);
+        drug.setDosageForm(oldConceptWithDosageForm);
+        saveDrugToConceptService(drug);
+
+        Integer oldDosageFormConceptIdInDrug = drug.getDosageForm().getConceptId();
+        assertThat(oldDosageFormConceptIdInDrug, is(7));
+
+        MergeConceptsService service = Context.getService(MergeConceptsService.class);
+        service.updateDrugs(oldConceptWithDosageForm, newConceptWithDosageForm);
+
+        Integer updatedDosageFormConceptIdInDrug = drug.getDosageForm().getConceptId();
+        assertThat(updatedDosageFormConceptIdInDrug, is(8));
+    }
+
+
+    private Concept getConceptFromConceptId(int conceptId) {
+        return Context.getConceptService().getConcept(conceptId);
+    }
+
+
+    private Drug saveDrugToConceptService(Drug drug) {
+        return Context.getConceptService().saveDrug(drug);
+    }
+
+
+    private Drug getDrugById(int drugId) {
+        return Context.getConceptService().getDrug(drugId);
+    }
+
+
+    private Order updateOrderWithConcept(int orderId, int conceptId) {
+        OrderService orderService = Context.getOrderService();
+        Order knownOrder = orderService.getOrder(orderId);
+        knownOrder.setConcept(Context.getConceptService().getConcept(conceptId));
+        return orderService.saveOrder(knownOrder);
+    }
+
+
+    private Obs updateObsWithConceptAnswer(int obsId, int conceptId) {
+        ObsService obsService = Context.getObsService();
+        Obs knownAnswerObs = obsService.getObs(obsId);
+        knownAnswerObs.setValueCoded(Context.getConceptService().getConcept(conceptId));
+        return obsService.saveObs(knownAnswerObs,"");
+    }
+
+
+    private Obs updateObsWithConceptQuestion(int obsId, int conceptId) {
+        ObsService obsService = Context.getObsService();
+        Obs knownAnswerObs = obsService.getObs(obsId);
+        knownAnswerObs.setConcept(Context.getConceptService().getConcept(conceptId));
+        return obsService.saveObs(knownAnswerObs,"");
+    }
 }
